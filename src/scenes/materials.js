33bf9fbc282516b6c1d04b13b06e2eada925c4ee
lalanneloicpierre <loic.lalanne@gmail.com>
2021-01-30 21:43:42 +0100

    Adding materials attribution script

---
+++ src/scenes/materials.js
@@ -0,0 +1,9 @@
+function manageMazeMaterial ( mesh ) {
+
+	debug('ouech')
+
+    let mazeMaterial = new BABYLON.StandardMaterial("mazeMaterial", runningScene);
+    mazeMaterial.diffuseColor = new BABYLON.Color3(1, 0.5, 0.5);
+
+    mesh.material = mazeMaterial;
+} 
