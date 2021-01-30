57217569ebff84d1112904def26543dcb0e5d0e3
lalanneloicpierre <loic.lalanne@gmail.com>
2021-01-30 21:48:46 +0100

    Update materials.js

--- src/scenes/materials.js
+++ src/scenes/materials.js
@@ -1,6 +1,6 @@
-function manageMazeMaterial ( mesh ) {
+function manageMazeMaterial ( mesh, scene ) {
 
-    let mazeMaterial = new BABYLON.StandardMaterial("mazeMaterial", runningScene);
+    let mazeMaterial = new BABYLON.StandardMaterial("mazeMaterial", scene);
     mazeMaterial.diffuseColor = new BABYLON.Color3(1, 0.5, 0.5);
 
     mesh.material = mazeMaterial;
