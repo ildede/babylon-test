6eb9cb2c4a36c1574d2743b5396cbfc7a12481ec
lalanneloicpierre <loic.lalanne@gmail.com>
2021-01-31 12:04:29 +0100

    Update materials.ts

--- src/scenes/functions/materials.ts
+++ src/scenes/functions/materials.ts
@@ -3,50 +3,107 @@ import {Scene} from "@babylonjs/core/scene";
 
 export default function manageMaterials(meshes: AbstractMesh[], scene: Scene): void {
 
-    const meshesGroup: {maze: Nullable<AbstractMesh>, walls: AbstractMesh[], canvas: AbstractMesh[]} = {
-        maze: scene.getMeshByName('maze'),
-        walls: [],
-        canvas: []
+    const meshesGroup: {
+        maze: Nullable<AbstractMesh>,
+        endDoor: Nullable<AbstractMesh>,
+        walls: AbstractMesh[],
+        canvas: AbstractMesh[],
+        canvasMarks: AbstractMesh[],
+        pathsMarks: AbstractMesh[]
+    } = {
+        maze : scene.getMeshByName('maze'),
+        endDoor : scene.getMeshByName('end_door'),
+        walls : [],
+        canvas : [],
+        canvasMarks : [],
+        pathsMark : [],
     };
 
     meshes.forEach((mesh) => {
+        
         if (mesh.name.startsWith('wall')) {
+            
             meshesGroup.walls.push(mesh);
+            
         } else if (mesh.name.startsWith('canva')) {
+            
+            meshesGroup.canvas.push(mesh);
+            
+        } else if (mesh.name.startsWith('canva_mark')) {
+            
+            meshesGroup.canvas.push(mesh);
+            
+        } else if (mesh.name.startsWith('path_mark')) {
+            
             meshesGroup.canvas.push(mesh);
         }
     });
 
     manageMazeMaterial(meshesGroup.maze, scene);
+    manageEndDoorMaterial(meshesGroup.endDoor, scene);
     manageWallsMaterial(meshesGroup.walls, scene);
     manageCanvasMaterial(meshesGroup.canvas, scene);
+    manageCanvasMarksMaterial(meshesGroup.canvasMarks, scene);
+    managePathsMarksMaterial(meshesGroup.pathsMark, scene);
 }
 
 function manageMazeMaterial(mesh: Nullable<AbstractMesh>, scene: Scene): void {
+    
     if (mesh) {
+        
         const mazeMaterial = new StandardMaterial("mazeMaterial", scene);
         mazeMaterial.diffuseColor = new Color3(1, 0.5, 0.5);
+        
         mesh.material = mazeMaterial;
     }
 }
 
+function manageEndDoorMaterial(mesh: Nullable<AbstractMesh>, scene: Scene): void {
+
+    if (mesh) {
+        
+        const doorMaterial = new StandardMaterial("doorMaterial", scene);
+        doorMaterial.diffuseColor = new Color3(0.5, 0.5, 1);
+
+        mesh.material = doorMaterial);
+    }
+}
+
 function manageWallsMaterial(meshes: AbstractMesh[], scene: Scene): void {
-
+    
     const wallMaterial = new StandardMaterial("wallMaterial", scene);
-    wallMaterial.diffuseColor = new Color3(1, 0.5, 0.5);
-
-    meshes.forEach((mesh) => mesh.material = wallMaterial);
+        wallMaterial.diffuseColor = new BABYLON.Color3(1, 0.5, 0.5);
+    
+    meshes.forEach((mesh) => { mesh.material = wallMaterial; });
 }
 
 function manageCanvasMaterial(meshes: AbstractMesh[], scene: Scene): void {
-
-    meshes.forEach((mesh) => {
-        const canvasMaterial = new StandardMaterial("canvasMaterial", scene);
+    
+    const canvasMaterial = new StandardMaterial("canvasMaterial", scene);
         // canvasMaterial.diffuseColor = new Color3(0.5, 1, 0.5);
         canvasMaterial.diffuseTexture = new Texture("/public/sprites/"+getTextureName(mesh.name), scene);
+    
+    meshes.forEach((mesh) => {
+        
         mesh.isPickable = true;
         mesh.material = canvasMaterial;
     });
+}
+
+function manageCanvasMarksMaterial(meshes: AbstractMesh[], scene: Scene): void {
+    
+    const canvaMarkMaterial = new StandardMaterial("wallMaterial", scene);
+        canvaMarkMaterial.diffuseColor = new BABYLON.Color3(0.5, 1, 0.5);
+    
+    meshes.forEach((mesh) => { mesh.material = canvaMarkMaterial; });
+}
+
+function managePathsMarksMaterial(meshes: AbstractMesh[], scene: Scene): void {
+    
+    const pathMarkMaterial = new StandardMaterial("wallMaterial", scene);
+        pathMarkMaterial.diffuseColor = new BABYLON.Color3(1, 1, 0.5);
+    
+    meshes.forEach((mesh) => { mesh.material = pathMarkMaterial; });
 }
 
 function getTextureName(s: string): string {
