1a67c99f0d77f9b953884df5cd91ff0b4e132ff0
Davide Pedron <davide@happydev.cloud>
2021-01-31 04:57:25 +0100

    use forEach

--- src/scenes/functions/materials.ts
+++ src/scenes/functions/materials.ts
@@ -1,26 +1,21 @@
 import {AbstractMesh, Color3, Nullable, StandardMaterial} from "@babylonjs/core";
 import {Scene} from "@babylonjs/core/scene";
 
-export default function manageMaterials ( meshes: AbstractMesh[], scene: Scene ): void {
+export default function manageMaterials(meshes: AbstractMesh[], scene: Scene): void {
 
-    const meshesLength = meshes.length
     const meshesGroup: {maze: Nullable<AbstractMesh>, walls: AbstractMesh[], canvas: AbstractMesh[]} = {
         maze: scene.getMeshByName('maze'),
         walls: [],
         canvas: []
     };
 
-    for(let i = 0; i < meshesLength; i++) {
-
-        if(meshes[i].name.substr(0, 4) == 'wall') {
-
-            meshesGroup.walls.push(meshes[i]);
-
-        } else if(meshes[i].name.substr(0, 5) == 'canva') {
-
-            meshesGroup.canvas.push(meshes[i]);
+    meshes.forEach((mesh) => {
+        if (mesh.name.startsWith('wall')) {
+            meshesGroup.walls.push(mesh);
+        } else if (mesh.name.startsWith('canva')) {
+            meshesGroup.canvas.push(mesh);
         }
-    }
+    });
 
     manageMazeMaterial(meshesGroup.maze, scene);
     manageWallsMaterial(meshesGroup.walls, scene);
@@ -28,31 +23,25 @@ export default function manageMaterials ( meshes: AbstractMesh[], scene: Scene )
 }
 
 function manageMazeMaterial(mesh: Nullable<AbstractMesh>, scene: Scene): void {
-
-    const mazeMaterial = new StandardMaterial("mazeMaterial", scene);
-    mazeMaterial.diffuseColor = new Color3(1, 0.5, 0.5);
-
     if (mesh) {
+        const mazeMaterial = new StandardMaterial("mazeMaterial", scene);
+        mazeMaterial.diffuseColor = new Color3(1, 0.5, 0.5);
         mesh.material = mazeMaterial;
     }
 }
 
-function manageWallsMaterial ( meshes: AbstractMesh[], scene: Scene ): void {
-
-    const meshesLength = meshes.length;
+function manageWallsMaterial(meshes: AbstractMesh[], scene: Scene): void {
 
     const wallMaterial = new StandardMaterial("wallMaterial", scene);
     wallMaterial.diffuseColor = new Color3(1, 0.5, 0.5);
 
-    for(let i = 0; i < meshesLength; i++) { meshes[i].material = wallMaterial; }
+    meshes.forEach((mesh) => mesh.material = wallMaterial);
 }
 
 function manageCanvasMaterial ( meshes: AbstractMesh[], scene: Scene ): void {
 
-    const meshesLength = meshes.length;
+    const canvasMaterial = new StandardMaterial("canvasMaterial", scene);
+    canvasMaterial.diffuseColor = new Color3(0.5, 1, 0.5);
 
-    const wallMaterial = new StandardMaterial("wallMaterial", scene);
-    wallMaterial.diffuseColor = new Color3(0.5, 1, 0.5);
-
-    for(let i = 0; i < meshesLength; i++) { meshes[i].material = wallMaterial; }
+    meshes.forEach((mesh) => mesh.material = canvasMaterial);
 }
